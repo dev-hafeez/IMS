caa44c5b1fe48a9543d3d02cebbab15a8b50bbd4
ali-ahmed03 <aleeahmed468@gmail.com>
2024-05-26 20:56:20 +0500

    Update Customer.java

--- src/main/java/com/mycompany/inventorymanagementsystem/Customer.java
+++ src/main/java/com/mycompany/inventorymanagementsystem/Customer.java
@@ -16,7 +16,7 @@ public class Customer extends JFrame {
         setLayout(new GridBagLayout());
         
         JLabel selectOption = new JLabel("Please select any option");
-        selectOption.setFont(new Font("Arial", Font.BOLD, 25));
+        selectOption.setFont(new Font("Calibri", Font.BOLD, 25));
         JButton buyProductButton = new JButton("Buy Product");
         JButton seeProductsButton = new JButton("See Products");
         JButton backButton = new JButton("Back");
